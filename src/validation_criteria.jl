3e99c0310d3b29dfc7a395602d24f8006f67e035
Vilen Jumutc <jumutc@gmail.com>
2015-04-12 21:57:37 +0200

    output model fixed

--- src/validation_criteria.jl
+++ src/validation_criteria.jl
@@ -1,2 +1,2 @@
 validation_criteria(model::SALSAModel,X,Y,val_idx) = validation_criteria(model,X[val_idx,:],Y[val_idx])
-validation_criteria(model::SALSAModel,X,Y) = (model.sparsity_cv*mean(model.w .!= 0) + (1-model.sparsity_cv)*misclass(Y, predict_raw(model,X))) / model.num_cv_folds
+validation_criteria(model::SALSAModel,X,Y) = (model.sparsity_cv*mean(model.output.w .!= 0) + (1-model.sparsity_cv)*misclass(Y, predict_raw(model,X))) / model.num_cv_folds
