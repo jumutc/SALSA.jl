7b503183a1724d10b4c281c9b15bb15215ff1f6b
Vilen Jumutc <jumutc@gmail.com>
2015-06-08 16:45:31 +0200

    fix for test randomness on different environments

--- test/functional/test.jl
+++ test/functional/test.jl
@@ -4,20 +4,20 @@ ripley = matread(joinpath(Pkg.dir("SALSA"),"data","ripley.mat"))
 
 srand(1234)
 model = salsa(LINEAR,PEGASOS,HINGE,ripley["X"],ripley["Y"],ripley["Xt"])
-@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.888 0.01
+@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.89 0.01
 
 srand(1234)
-model = SALSAModel(LINEAR,PEGASOS,PINBALL,global_opt=DS([0,-5]))
+model = SALSAModel(LINEAR,PEGASOS,PINBALL,global_opt=DS([0,-1]))
 model = salsa(ripley["X"],ripley["Y"],model,ripley["Xt"])
-@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.881 0.01
+@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.89 0.01
 
 srand(1234)
-model = SALSAModel(LINEAR,PEGASOS,LEAST_SQUARES,global_opt=DS([-5]))
+model = SALSAModel(LINEAR,PEGASOS,LEAST_SQUARES,global_opt=DS([-1]))
 model = salsa(ripley["X"],ripley["Y"],model,ripley["Xt"])
 @test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.885 0.01
 
 srand(1234)
-model = SALSAModel(LINEAR,PEGASOS,LOGISTIC,global_opt=DS([-5]))
+model = SALSAModel(LINEAR,PEGASOS,LOGISTIC,global_opt=DS([-1]))
 model = salsa(ripley["X"],ripley["Y"],model,ripley["Xt"])
 @test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.887 0.01
 
