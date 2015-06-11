b1277da4b8976fce881559249b3015e15836e477
Vilen Jumutc <jumutc@gmail.com>
2015-06-11 12:10:30 +0200

    new tests added

--- test/functional/test_nonlinear.jl
+++ test/functional/test_nonlinear.jl
@@ -9,12 +9,12 @@ model = salsa(NONLINEAR,PEGASOS,HINGE,ripley["X"],ripley["Y"],ripley["Xt"])
 srand(1234)
 model = SALSAModel(NONLINEAR,PEGASOS(),LOGISTIC,global_opt=DS([-1,-1,1]),kernel=PolynomialKernel)
 model = salsa(ripley["X"],ripley["Y"],model,ripley["Xt"])
-@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.888 0.01
+@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.895 0.01
 
 srand(1234)
 model = SALSAModel(NONLINEAR,PEGASOS(),LOGISTIC,global_opt=DS([-1]),kernel=LinearKernel)
 model = salsa(ripley["X"],ripley["Y"],model,ripley["Xt"])
-@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.88 0.01
+@test_approx_eq_eps mean(ripley["Yt"] .== model.output.Ytest) 0.89 0.01
 
 srand(1234)
 model = SALSAModel(NONLINEAR,DROP_OUT(),HINGE,global_opt=DS([-5,1]))
