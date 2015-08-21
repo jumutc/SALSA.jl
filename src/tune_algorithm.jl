7b8306efa5927a38c22e32afcfe94ee44bafa205
vilen <jumutc@gmail.com>
2015-08-21 23:26:51 +0200

    REQUIRE updated

--- src/tune_algorithm.jl
+++ src/tune_algorithm.jl
@@ -1,6 +1,6 @@
 function tune_algorithm(X, Y, model::SALSAModel)
     cost_fun = x0 -> cross_validate_algorithm(x0,X,Y,model)
-    par = run_global_opt(model,cost_fun, model.global_opt)
+    par = run_global_opt(model,cost_fun,model.global_opt,(5*size(Y,2),5))
     
     # generate model from the parameters
     model.output.mode = LINEAR()
@@ -8,6 +8,7 @@ function tune_algorithm(X, Y, model::SALSAModel)
 end
 
 function cross_validate_algorithm(x0, X, Y, model)
+    for 
     # generate model from parameters
     model = model_from_parameters(model,x0)
     # perform Kfold cross-validation by a generic and parallelizable function
@@ -18,18 +19,18 @@ function cross_validate_algorithm(x0, X, Y, model)
     end
 end
 
-function run_global_opt(model::SALSAModel, cost_fun::Function, global_opt::CSA; n_params=5)
+function run_global_opt(model::SALSAModel, cost_fun::Function, global_opt::CSA, par_dims)
     # Coupled Simulated Annealing calculations
     eval_fun = pars -> [cost_fun(pars[:,i]) for i=1:size(pars,2)]
-    (fval, par) = csa(eval_fun, randn(n_params,5))
+    (fval, par) = csa(eval_fun, randn(par_dims))
     @printf "CSA results: optimal %s = %.3f\n" validation_criteria(model.validation_criteria, model) fval
     return par
 end 
 
-function run_global_opt(model::SALSAModel, cost_fun::Function, global_opt::DS; n_params=5)
+function run_global_opt(model::SALSAModel, cost_fun::Function, global_opt::DS, par_dims)
     # Randomized Directional Search calculations
     params = global_opt.init_params
-    (fval, par) = ds(cost_fun, isempty(params) ? randn(n_params) : params)
+    (fval, par) = ds(cost_fun, isempty(params) ? randn(par_dims[1]) : params)
     @printf "DS results: optimal %s = %.3f\n" validation_criteria(model.validation_criteria, model) fval
     return par
 end 
