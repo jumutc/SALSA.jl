79995127ab2180808dfdb18287c7f171dcb94bea
Vilen Jumutc <jumutc@gmail.com>
2015-09-01 12:33:31 +0200

    build fix

---
+++ test/functional/qa_tables/test_qa.jl
@@ -0,0 +1,23 @@
+using SALSA, Base.Test
+
+outOriginal = STDOUT
+(outRead, outWrite) = redirect_stdout()
+
+read_char = () -> '\n'
+read_int = () -> '\n'
+
+Xf = readcsv(joinpath(Pkg.dir("SALSA"),"data","iris.data.csv"))
+show(salsa_qa(Xf,read_char,read_int))
+
+s = utf8(readavailable(outRead))
+redirect_stdout(outOriginal)
+
+#test Q/A table
+@test contains(s,"SALSA.HINGE")
+@test contains(s,"SALSA.PEGASOS")
+@test contains(s,"SALSA.LINEAR")
+@test contains(s, "(or ENTER for default)")
+# test output model 
+@test contains(s,"SALSA model:")
+@test contains(s,"SALSA model.output:")
+@test contains(s,"dfunc")
