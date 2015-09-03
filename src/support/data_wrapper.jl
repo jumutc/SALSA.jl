fa18dd1eb7db0116527d1054e6fc230214da820c
Vilen Jumutc <jumutc@gmail.com>
2015-09-03 13:47:47 +0200

    build fix

--- src/support/data_wrapper.jl
+++ src/support/data_wrapper.jl
@@ -32,6 +32,7 @@ end
 # fix for julia release where this function is absent, TODO: remove when we move to julia 0.4+
 sub(a::SubArray, I::AbstractVector, ::Colon) = convert(Array, a[I,:])
 sub(a::AbstractMatrix, I::AbstractVector, ::Colon) = a[I,:]
+sub(a::AbstractVector, I::AbstractVector, ::Colon) = a[I]
 sub(a::AbstractMatrix, i::Int, ::Colon) = a[i,:]
 sub(a::AbstractMatrix, ::Colon, ::Colon) = a
 
